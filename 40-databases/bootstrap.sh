8525d9fbaeec1f792cec834ffc2af522e880d063
Manikanta Akula <maniakula202@gmail.com>
2025-11-01 23:21:10 +0530

    Terrafrom Practice

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 component=$1
 dnf install ansible -y
-ansible-pull -U https://github.com/daws-86s/ansible-roboshop-roles-tf.git component=$component main.yaml
+ansible-pull -U https://github.com/Maniakula202/ansible-roboshop-roles-tf.git component=$component main.yaml
