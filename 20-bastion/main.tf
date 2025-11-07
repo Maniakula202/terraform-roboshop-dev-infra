f648f343a61c4c9242e1d67a49e7d8133a60e58d
Manikanta Akula <maniakula202@gmail.com>
2025-11-07 22:09:11 +0530

    Terraform Practice

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -3,12 +3,26 @@ resource "aws_instance" "bastion" {
     instance_type = "t3.micro"
     vpc_security_group_ids = [local.bastion_sg_id]
     subnet_id = local.public_subnet_id
-    user_data = file("bastion.sh")
+    iam_instance_profile = aws_iam_instance_profile.bastion.name
+
+    root_block_device {
+        volume_size = 50
+        volume_type = "gp3" # or "gp2", depending on your preference
+    }
     
+    user_data = file("bastion.sh")
+
     tags = merge (
         local.common_tags,
         {
             Name = "${var.project_name}-${var.environment}-bastion"
         }
     )
+}
+
+# Assigning Terraform admin roe to the bastion server
+
+resource "aws_iam_instance_profile" "bastion" {
+    name = "bastion"
+    role = "BastionTerraformAdmin"
 }
--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -167,3 +167,39 @@ resource "terraform_data" "mysql" {
     ]
   }
 }  
+
+resource "aws_route53_record" "mongodb" {
+  zone_id = var.zone_id
+  name    = "mongodb-${environment}.${domain_name}"  # mongodb-dev.manidevops.fun
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.mongodb.private_ip]
+  allow_overwrite = true
+}
+
+resource "aws_route53_record" "redis" {
+  zone_id = var.zone_id
+  name    = "redis-${environment}.${domain_name}"  # redis-dev.manidevops.fun
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.redis.private_ip]
+  allow_overwrite = true
+}
+
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = var.zone_id
+  name    = "rabbitmq-${environment}.${domain_name}"  # rabbitmq-dev.manidevops.fun
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.rabbitmq.private_ip]
+  allow_overwrite = true
+}
+
+resource "aws_route53_record" "mysql" {
+  zone_id = var.zone_id
+  name    = "mysql-${environment}.${domain_name}"  # mysql-dev.manidevops.fun
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.mysql.private_ip]
+  allow_overwrite = true
+}
