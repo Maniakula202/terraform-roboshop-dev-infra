fd49e0f286660db8d6b13a5e6e6e004a3db205ed
Manikanta Akula <maniakula202@gmail.com>
2025-11-07 22:44:19 +0530

    Terraform Practice

---
+++ 60-catalogue/locals.tf
@@ -0,0 +1,12 @@
+locals {
+    ami_id = data.aws_ami.joindevops
+    catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
+    private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids)[0]
+    common_tags = {
+        Project = var.project_name
+        Environment = var.environment
+        Terraform = "true"
+    }
+}
+
+
