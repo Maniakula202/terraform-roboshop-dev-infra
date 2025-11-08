7b44c1fa57963ba6ea6bf17df16058359304765e
Manikanta Akula <maniakula202@gmail.com>
2025-11-08 21:11:14 +0530

    Terraform Practice

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -2,6 +2,7 @@ locals {
     ami_id = data.aws_ami.joindevops.id
     catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
     vip_id = data.aws_ssm_parameter.vip_id.value
+    backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
     private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
     private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
     common_tags = {
