bb64e5a7886fa3609091e7388bd28a5bbe97309c
Manikanta Akula <maniakula202@gmail.com>
2025-11-10 22:15:47 +0530

    Terraform Practice

--- 50-backend_alb/variables.tf
+++ 50-backend_alb/variables.tf
@@ -5,3 +5,11 @@ variable "project_name" {
 variable "environment" {
     default = "dev"
 }
+
+variable "zone_id" {
+    default = "Z04762511YRI8TCYP5ZSP"
+}
+
+variable "domain_name" {
+    default = "manidevops.fun"
+}
